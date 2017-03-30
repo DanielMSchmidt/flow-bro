9098866143b538dda8c9f3f6a908f7bb87f7126c
Daniel Schmidt <danielmschmidt92@gmail.com>
2017-03-30 16:00:10 +0200

    0.0.3-3

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
 var cli = require('commander');
+var fs = require('fs');
 var getUntyped = require('./get-untyped');
 
 const checkForFlowToExist = () => {
-    // ./node_modules/.bin/flow
+    fs.existsSync('./node_modules/.bin/flow');
 };
 
 cli
@@ -15,18 +16,21 @@ cli
 
         const amount = rawAmount ? parseInt(rawAmount) : 20;
 
-        getUntyped(amount).then(files => {
-            console.log(files);
-            // console.log(
-            //     'The following files are the ones with the least flow coverage: \n'
-            // );
-            // console.log(
-            //     files
-            //         .map(file => `${file.file} is ${file.result}% typed`)
-            //         .join('\n')
-            // );
-            // console.log('\n I am sure you can improve them!');
-        });
+        getUntyped(amount)
+            .then(files => {
+                console.log(
+                    'The following files are the ones with the least flow coverage: \n'
+                );
+                console.log(
+                    files
+                        .map(file => `${file.file} is ${file.result}% typed`)
+                        .join('\n')
+                );
+                console.log('\n I am sure you can improve them!');
+            })
+            .catch(() => {
+                console.error('Something went wrong, I am sorry :/');
+            });
     });
 
 cli.parse(process.argv);
