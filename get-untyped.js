24dba16a3c02aae2bbc4b9f91e9671c5b22d35a3
Daniel Schmidt <danielmschmidt92@gmail.com>
2017-03-30 16:57:16 +0200

    WIP

--- get-untyped.js
+++ get-untyped.js
@@ -13,15 +13,15 @@ function toPromise(fn, ...args) {
     });
 }
 
-function getFlowCoverage(filePath) {
+function getFlowCoverage(filePathPromise) {
     const startValue = 'Covered:';
     const endValue = '\% (';
 
-    return toPromise(execFile, './node_modules/.bin/flow', [
-        'coverage',
-        filePath,
-    ])
-        .then(result => {
+    filePathPromise.then(filePath =>
+        toPromise(execFile, './node_modules/.bin/flow', [
+            'coverage',
+            filePath,
+        ]).then(result => {
             const startOfValue = result.indexOf(startValue) +
                 startValue.length +
                 1;
@@ -35,8 +35,7 @@ function getFlowCoverage(filePath) {
                 file: filePath,
                 result: parseFloat(result.substring(startOfValue, endOfValue)),
             };
-        })
-        .catch(err => {});
+        }));
 }
 
 module.exports = function(amount) {
@@ -44,8 +43,10 @@ module.exports = function(amount) {
         .then(fileString => fileString.split('\n'))
         .then(files => files.filter(file => file !== ''))
         .then(files => files.filter(file => file.indexOf('.js') !== -1))
+        .then(files =>
+            files.map(file => new ThrottledPromise(resolve => resolve(file))))
         .then(files => files.map(file => getFlowCoverage(file)))
-        .then(filePromises => Promise.all(filePromises))
+        .then(filePromises => ThrottledPromise.all(filePromises, 4))
         .then(files => files.sort((a, b) => a.result - b.result))
         .then(files => files.slice(0, amount));
 };
