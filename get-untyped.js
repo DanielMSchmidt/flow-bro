d4e3c62fdf263e865d23737d4a370c095d69c9d3
Daniel Schmidt <danielmschmidt92@gmail.com>
2017-03-30 17:08:42 +0200

    WIP

--- get-untyped.js
+++ get-untyped.js
@@ -14,29 +14,26 @@ function toPromise(fn, ...args) {
     });
 }
 
-function getFlowCoverage(filePathPromise) {
+function getFlowCoverage(filePath) {
     const startValue = 'Covered:';
     const endValue = '\% (';
 
-    filePathPromise.then(filePath =>
-        toPromise(execFile, './node_modules/.bin/flow', [
-            'coverage',
-            filePath,
-        ]).then(result => {
-            const startOfValue = result.indexOf(startValue) +
-                startValue.length +
-                1;
-            const endOfValue = result.indexOf(endValue);
+    return toPromise(execFile, './node_modules/.bin/flow', [
+        'coverage',
+        filePath,
+    ]).then(result => {
+        const startOfValue = result.indexOf(startValue) + startValue.length + 1;
+        const endOfValue = result.indexOf(endValue);
 
-            if (startOfValue === -1 || endOfValue === -1) {
-                throw 'wrong format';
-            }
+        if (startOfValue === -1 || endOfValue === -1) {
+            throw 'wrong format';
+        }
 
-            return {
-                file: filePath,
-                result: parseFloat(result.substring(startOfValue, endOfValue)),
-            };
-        }));
+        return {
+            file: filePath,
+            result: parseFloat(result.substring(startOfValue, endOfValue)),
+        };
+    });
 }
 
 module.exports = function(amount) {
@@ -45,8 +42,11 @@ module.exports = function(amount) {
         .then(files => files.filter(file => file !== ''))
         .then(files => files.filter(file => file.indexOf('.js') !== -1))
         .then(files =>
-            files.map(file => new ThrottledPromise(resolve => resolve(file))))
-        .then(files => files.map(file => getFlowCoverage(file)))
+            files.map(
+                file =>
+                    new ThrottledPromise(resolve =>
+                        getFlowCoverage(file).then(result => resolve(result)))
+            ))
         .then(filePromises => ThrottledPromise.all(filePromises, 4))
         .then(files => files.sort((a, b) => a.result - b.result))
         .then(files => files.slice(0, amount))
