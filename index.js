9463f2b21e7d5c1808fc7c51c466082072a004dd
Daniel Schmidt <danielmschmidt92@gmail.com>
2017-03-30 15:27:26 +0200

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+var cli = require('commander');
+var getUntyped = require('./get-untyped');
+
+cli
+    .command('get-untyped <number>')
+    .description('returns the X least flow-typed files')
+    .action(function(rawAmount) {
+        const amount = rawAmount ? parseInt(rawAmount) : 20;
+
+        getUntyped(amount).then(files => {
+            console.log(
+                'The following files are the ones with the least flow coverage: \n'
+            );
+            console.log(
+                files
+                    .map(file => `${file.file} is ${file.result}% typed`)
+                    .join('\n')
+            );
+            console.log('\n I am sure you can improve them!');
+        });
+    });
+
+program.parse(process.argv);
