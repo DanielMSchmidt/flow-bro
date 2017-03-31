97488e3d80ebbc5adaf859f220280a1356db0d20
Daniel Schmidt <danielmschmidt92@gmail.com>
2017-03-31 11:37:39 +0200

    add test file

---
+++ testfile.js
@@ -0,0 +1,20 @@
+import React, { PropTypes } from 'react';
+
+EventsScreen.propTypes = {
+    ass: PropTypes.shape({
+        fo: PropTypes.shape({
+            asd: PropTypes.arrayOf(
+                PropTypes.shape({
+                    ccc: PropTypes.string.isRequired,
+                    asd: PropTypes.shape({
+                        yxc: PropTypes.string.isRequired,
+                    }).isRequired,
+                    yxcxcy: PropTypes.number.isRequired,
+                    yxcxcy12: PropTypes.boolean.isRequired,
+                }),
+            ).isRequired,
+        }).isRequired,
+    }),
+    asasas: PropTypes.func.isRequired,
+    qwe: PropTypes.func,
+};
