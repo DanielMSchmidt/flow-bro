abbd21edf1b7bea94271bc0f2051a3d41840c219
Daniel Schmidt <danielmschmidt92@gmail.com>
2017-04-05 11:18:11 +0200

    WIP

--- index.js
+++ index.js
@@ -14,47 +14,47 @@ cli
     .command('get-untyped <number>')
     .description('returns the X least flow-typed files')
     .action(function(rawAmount) {
-      if (!checkForFlowToExist()) {
-          console.log('We expect a local version of flow');
-          return;
-      }
-      const amount = rawAmount ? parseInt(rawAmount) : 20;
+        if (!checkForFlowToExist()) {
+            console.log('We expect a local version of flow');
+            return;
+        }
+        const amount = rawAmount ? parseInt(rawAmount) : 20;
 
-      getUntyped(amount)
-          .then(files => {
-              console.log(
-                  'The following files have the least flow coverage: \n'
-              );
-              var table = new Table({
-                  head: ['%', 'File'],
-                  colWidths: [10, 80],
-              });
-              files.forEach(({ file, result }) => table.push([result, file]));
+        getUntyped(amount)
+            .then(files => {
+                console.log(
+                    'The following files have the least flow coverage: \n'
+                );
+                var table = new Table({
+                    head: ['%', 'File'],
+                    colWidths: [10, 80],
+                });
+                files.forEach(({ file, result }) => table.push([result, file]));
 
-              console.log(table.toString());
+                console.log(table.toString());
 
-              console.log('\nI am sure you can improve them!');
-          })
-          .catch(error => {
-              console.error('Something went wrong, I am sorry:');
-              console.error(error);
-          });
+                console.log('\nI am sure you can improve them!');
+            })
+            .catch(error => {
+                console.error('Something went wrong, I am sorry:');
+                console.error(error);
+            });
     });
 
 cli
-  .command('project-coverage')
-  .description('Gets your overall coverae');
-  .action(function() {
-    if (!checkForFlowToExist()) {
-        console.log('We expect a local version of flow');
-        return;
-    }
+    .command('project-coverage')
+    .description('Gets your overall coverage')
+    .action(function() {
+        if (!checkForFlowToExist()) {
+            console.log('We expect a local version of flow');
+            return;
+        }
 
-    getProjectCoverage().then(({covered, all}) => {
-      console.log('You have a coverage of', covered / all, '%\n');
-      console.log('Covered Lines:', covered);
-      console.log('Total Lines:', all);
+        getProjectCoverage().then(({ covered, all }) => {
+            console.log('You have a coverage of', covered / all, '%\n');
+            console.log('Covered Lines:', covered);
+            console.log('Total Lines:', all);
+        });
     });
-  })
 
 cli.parse(process.argv);
