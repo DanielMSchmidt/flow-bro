dc5fbba48e6908644245fdd75048948fb04224fd
Daniel Schmidt <danielmschmidt92@gmail.com>
2017-04-21 11:16:46 +0200

    feat: watch mode

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 
 const cli = require('commander');
 const fs = require('fs');
+const Table = require('cli-table');
+
 const getUntyped = require('./get-untyped');
 const getProjectCoverage = require('./get-project-coverage');
-const Table = require('cli-table');
+const flowStatus = require('./flow-status');
+const stopFlowServer = require('./flow-stop');
 
 const checkForFlowToExist = () => {
     return fs.existsSync('./node_modules/.bin/flow');
@@ -27,7 +30,7 @@ cli
                 );
                 var table = new Table({
                     head: ['%', 'File'],
-                    colWidths: [10, 80],
+                    colWidths: [10, 80]
                 });
                 files.forEach(({ file, result }) => table.push([result, file]));
 
@@ -55,6 +58,43 @@ cli
             console.log('You have a coverage of', covered / all * 100, '%\n');
             console.log('Covered Lines:', covered);
             console.log('Total Lines:', all);
+        });
+    });
+
+cli
+    .command('watch')
+    .alias('w')
+    .description('Watches file changes and executes flow')
+    .action(function() {
+        if (!checkForFlowToExist()) {
+            console.log('We expect a local version of flow');
+            return;
+        }
+        const callFlow = () => {
+            flowStatus()
+                .then(result => {
+                    console.log(result);
+                })
+                .catch(({ stdout }) => {
+                    console.log('Dude, there was an error:\n');
+                    console.log(stdout);
+                });
+        };
+
+        console.log(
+            'Hey bro, I will start up the flow-server for you real quick!'
+        );
+        callFlow();
+        fs.watch('./', { persistent: true, recursive: true }, callFlow);
+
+        process.on('SIGINT', function() {
+            console.log(
+                '\nBye bro! I will just kill that flow-server real quick for you.'
+            );
+
+            stopFlowServer().then(() => {
+                process.exit();
+            });
         });
     });
 
