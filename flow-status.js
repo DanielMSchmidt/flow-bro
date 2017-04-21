74e6cd8830db14d18108aa7de2b1d9dc5363b51d
Daniel Schmidt <danielmschmidt92@gmail.com>
2017-04-21 11:16:31 +0200

    chore: add flow command helpers

---
+++ flow-status.js
@@ -0,0 +1,9 @@
+const execFile = require('child_process').execFile;
+const toPromise = require('./to-promise');
+
+module.exports = function flowStatus(options) {
+    return toPromise(execFile, './node_modules/.bin/flow', [
+        'status',
+        '--show-all-errors'
+    ]);
+};
