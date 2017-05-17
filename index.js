17abd0045103e8cacf0e5905a8b94cd893cc7810
Daniel Schmidt <danielmschmidt92@gmail.com>
2017-05-17 10:10:22 +0200

    add spinner for nicer loading interface

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const cli = require('commander');
 const fs = require('fs');
 const Table = require('cli-table');
+const clear = require('clear');
+const ora = require('ora');
 
 const getUntyped = require('./get-untyped');
 const getProjectCoverage = require('./get-project-coverage');
@@ -10,92 +12,113 @@ const flowStatus = require('./flow-status');
 const stopFlowServer = require('./flow-stop');
 
 const checkForFlowToExist = () => {
-    return fs.existsSync('./node_modules/.bin/flow');
+  return fs.existsSync('./node_modules/.bin/flow');
 };
 
 cli
-    .command('get-untyped <number>')
-    .description('returns the X least flow-typed files')
-    .action(function(rawAmount) {
-        if (!checkForFlowToExist()) {
-            console.log('We expect a local version of flow');
-            return;
-        }
-        const amount = rawAmount ? parseInt(rawAmount) : 20;
+  .command('get-untyped <number>')
+  .description('returns the X least flow-typed files')
+  .action(function(rawAmount) {
+    if (!checkForFlowToExist()) {
+      console.log('We expect a local version of flow');
+      return;
+    }
+    const amount = rawAmount ? parseInt(rawAmount) : 20;
 
-        getUntyped(amount)
-            .then(files => {
-                console.log(
-                    'The following files have the least flow coverage: \n'
-                );
-                var table = new Table({
-                    head: ['%', 'File'],
-                    colWidths: [10, 80]
-                });
-                files.forEach(({ file, result }) => table.push([result, file]));
+    getUntyped(amount)
+      .then(files => {
+        console.log('The following files have the least flow coverage: \n');
+        var table = new Table({
+          head: ['%', 'File'],
+          colWidths: [10, 80],
+        });
+        files.forEach(({ file, result }) => table.push([result, file]));
 
-                console.log(table.toString());
+        console.log(table.toString());
 
-                console.log('\nI am sure you can improve them!');
-            })
-            .catch(error => {
-                console.error('Something went wrong, I am sorry:');
-                console.error(error);
-            });
-    });
+        console.log('\nI am sure you can improve them!');
+      })
+      .catch(error => {
+        console.error('Something went wrong, I am sorry:');
+        console.error(error);
+      });
+  });
 
 cli
-    .command('project-coverage')
-    .alias('coverage')
-    .description('Gets your overall coverage')
-    .action(function() {
-        if (!checkForFlowToExist()) {
-            console.log('We expect a local version of flow');
-            return;
-        }
+  .command('project-coverage')
+  .alias('coverage')
+  .description('Gets your overall coverage')
+  .action(function() {
+    if (!checkForFlowToExist()) {
+      console.log('We expect a local version of flow');
+      return;
+    }
 
-        getProjectCoverage().then(({ covered, all }) => {
-            console.log('You have a coverage of', covered / all * 100, '%\n');
-            console.log('Covered Lines:', covered);
-            console.log('Total Lines:', all);
-        });
+    getProjectCoverage().then(({ covered, all }) => {
+      console.log('You have a coverage of', covered / all * 100, '%\n');
+      console.log('Covered Lines:', covered);
+      console.log('Total Lines:', all);
     });
+  });
 
 cli
-    .command('watch')
-    .alias('w')
-    .description('Watches file changes and executes flow')
-    .action(function() {
-        if (!checkForFlowToExist()) {
-            console.log('We expect a local version of flow');
+  .command('watch')
+  .alias('w')
+  .description('Watches file changes and executes flow')
+  .action(function() {
+    if (!checkForFlowToExist()) {
+      console.log('We expect a local version of flow');
+      return;
+    }
+    let terminating = false;
+    let spinner = null;
+
+    const renewDisplay = () => {
+      if (terminating) {
+        return;
+      }
+
+      clear();
+      if (!spinner) {
+        spinner = ora('Checking the flow').start();
+      }
+
+      flowStatus().then(
+        result => {
+          spinner.succeed('Looks great, good job!');
+          console.log(result);
+        },
+        ({ stdout }) => {
+          if (terminating) {
             return;
+          }
+
+          spinner.fail('Dude, there was an error. Go fix it!');
+          console.log(stdout);
         }
-        const callFlow = () => {
-            flowStatus()
-                .then(result => {
-                    console.log(result);
-                })
-                .catch(({ stdout }) => {
-                    console.log('Dude, there was an error:\n');
-                    console.log(stdout);
-                });
-        };
+      );
+    };
 
-        console.log(
-            'Hey bro, I will start up the flow-server for you real quick!'
-        );
-        callFlow();
-        fs.watch('./', { persistent: true, recursive: true }, callFlow);
+    const stop = function() {
+      terminating = true;
+      if (spinner) {
+        spinner.stop();
+      }
 
-        process.on('SIGINT', function() {
-            console.log(
-                '\nBye bro! I will just kill that flow-server real quick for you.'
-            );
+      clear();
+      spinner = ora(
+        'Bye bro! I will just kill that flow-server real quick for you.'
+      ).start();
 
-            stopFlowServer().then(() => {
-                process.exit();
-            });
-        });
-    });
+      stopFlowServer().then(() => {
+        spinner.succeed();
+        process.exit();
+      });
+    };
+
+    renewDisplay();
+    fs.watch('./', { persistent: true, recursive: true }, renewDisplay);
+    process.on('SIGINT', stop);
+  });
 
 cli.parse(process.argv);
